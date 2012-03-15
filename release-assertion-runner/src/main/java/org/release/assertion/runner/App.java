a22ea559c4c0ecf2a95ee1dff6d15857d51ffd47
vparekh <vparekh@1df66291-9572-4e14-8f18-50d9d9145325>
2012-03-15 20:40:48 +0000

    Added release-assertion-runner module to trunk.
    git-svn-id: https://csfe.aceworkspace.net/svn/repos/release-assertion-toolkit/trunk@70 1df66291-9572-4e14-8f18-50d9d9145325

---
+++ release-assertion-runner/src/main/java/org/release/assertion/runner/App.java
@@ -0,0 +1,13 @@
+package org.release.assertion.runner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
