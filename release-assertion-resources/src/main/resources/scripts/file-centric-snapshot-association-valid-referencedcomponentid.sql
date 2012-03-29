2a09aa5a07480140b6be624bd9c06424c26fb7e3
vparekh <vparekh@1df66291-9572-4e14-8f18-50d9d9145325>
2012-03-29 21:00:14 +0000

    Removing description id as well.
    git-svn-id: https://csfe.aceworkspace.net/svn/repos/release-assertion-toolkit/trunk@144 1df66291-9572-4e14-8f18-50d9d9145325

--- release-assertion-resources/src/main/resources/scripts/file-centric-snapshot-association-valid-referencedcomponentid.sql
+++ release-assertion-resources/src/main/resources/scripts/file-centric-snapshot-association-valid-referencedcomponentid.sql
@@ -8,15 +8,18 @@
 ********************************************************************************/
 	
 /* 	view of current snapshot made by finding invalid referencedcomponentid */
-	create or replace view v_curr_snapshot as
+	create table v_curr_snapshot as
 	select a.referencedcomponentid
 	from curr_associationrefset_s a
-	left join curr_description_s b
-	on (a.referencedcomponentid = b.id
-	or a.referencedcomponentid = b.conceptid)
-	where (b.id is null or b.conceptid is null);
+	left join curr_concept_s b
+	on a.referencedcomponentid = b.id
+	where b.id is null;
 	
-
+  
+	delete from v_curr_snapshot 
+	where referencedcomponentid in (
+	select id from curr_description_s);
+	
 	
 /* 	inserting exceptions in the result table */
 	insert into qa_result (runid, assertionuuid, assertiontext, details)
@@ -28,4 +31,4 @@
 	from v_curr_snapshot a;
 
 
-	drop view v_curr_snapshot;
+	drop table v_curr_snapshot;
