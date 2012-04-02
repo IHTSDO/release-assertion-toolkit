3499d6cdf4c6652e9469c629e4cf88d5cf509aea
jefron <jefron@1df66291-9572-4e14-8f18-50d9d9145325>
2012-04-02 21:38:17 +0000

    Non logic based fixes
    git-svn-id: https://csfe.aceworkspace.net/svn/repos/release-assertion-toolkit/trunk@156 1df66291-9572-4e14-8f18-50d9d9145325

--- release-assertion-resources/src/main/resources/scripts/component-centric/component-centric-snapshot-stated-inactive-modifier.sql
+++ release-assertion-resources/src/main/resources/scripts/component-centric/component-centric-snapshot-stated-inactive-modifier.sql
@@ -11,7 +11,7 @@
 		<RUNID>,
 		'<ASSERTIONUUID>',
 		'<ASSERTIONTEXT>',
-		concat('Relationship: id=',a.id, ': Stated relationship has a non 'SOME' modifier.') 	
+		concat('Relationship: id=',a.id, ': Stated relationship has a non -SOME- modifier.') 	
 	from curr_stated_relationship_s a
 	where a.modifierid != '900000000000451002'
 	
