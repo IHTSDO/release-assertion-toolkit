08c79cee768f7baf48bfd65e36edfc2033bbd6e8
vparekh <vparekh@1df66291-9572-4e14-8f18-50d9d9145325>
2012-04-04 20:42:57 +0000

    git-svn-id: https://csfe.aceworkspace.net/svn/repos/release-assertion-toolkit/trunk@170 1df66291-9572-4e14-8f18-50d9d9145325

---
+++ release-assertion-resources/src/main/resources/scripts/component-centric/component-centric-snapshot-description-case-word-match.sql
@@ -0,0 +1,45 @@
+
+/******************************************************************************** 
+	component-centric-snapshot-description-case-word-match
+
+	Assertion:
+	Case-sensitive terms that share initial words also share caseSignificanceId value.
+
+********************************************************************************/
+	
+/* 	view of current snapshot made by finding all the casesitive term for active concepts */
+
+
+	create or replace view v_curr_snapshot_1 as
+	select SUBSTRING_INDEX(term, ' ', 1) as firstword , a.conceptid , a.id , a.term , a.casesignificanceid
+	from  curr_description_s a , curr_concept_s b
+	where a.casesignificanceid = 900000000000017005
+	and a.active = 1
+	and b.active = 1
+	and a.conceptid = b.id;
+	
+	create or replace view v_curr_snapshot_2 as
+	select a.conceptid , a.term , b.term ,  a.casesignificanceid , b.casesignificanceid
+	from v_curr_snapshot_1 a , curr_description_s b , curr_concept_s c
+	where b.casesignificanceid = 900000000000020002
+	and b.active = 1
+	and c.active = 1
+	and b.conceptid = c.id
+	and a.conceptid = b.conceptid
+	and BINARY SUBSTRING_INDEX(b.term, ' ', 1) = firstword;	
+
+	
+/* 	inserting exceptions in the result table */
+	insert into qa_result (runid, assertionuuid, assertiontext, details)
+	select 
+		<RUNID>,
+		'<ASSERTIONUUID>',
+		'<ASSERTIONTEXT>',
+		concat('CONCEPT: id=',a.conceptid, ':Terms not sharing case-sensitivity.') 	
+	from v_curr_snapshot a;
+
+
+	drop view v_curr_snapshot_1;
+	drop view v_curr_snapshot_2;
+
+	
