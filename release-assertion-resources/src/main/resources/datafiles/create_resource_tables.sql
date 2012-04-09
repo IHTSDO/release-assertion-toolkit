24ca0d056938c88af56d12c53df466614643ee37
jefron <jefron@1df66291-9572-4e14-8f18-50d9d9145325>
2012-04-09 23:29:19 +0000

    Add resource datafiles GB-Terms and Navigation Concepts.  Also add a create_resource_tables.sql script
    git-svn-id: https://csfe.aceworkspace.net/svn/repos/release-assertion-toolkit/trunk@202 1df66291-9572-4e14-8f18-50d9d9145325

---
+++ release-assertion-resources/src/main/resources/datafiles/create_resource_tables.sql
@@ -0,0 +1,18 @@
+create table res_navigationconcepts 
+ (
+ id	varchar(36),
+ effectiveTime	varchar(255),
+ active	varchar(255),
+ moduleId	varchar(255),
+ refsetId	varchar(255),
+ referencedComponentId	varchar(255),
+ ordernum	varchar(255),
+ linkedTo	varchar(255)
+ );
+ 
+ create table res_gbterms 
+  (
+  term	varchar(255)
+  );
+  
+ 
