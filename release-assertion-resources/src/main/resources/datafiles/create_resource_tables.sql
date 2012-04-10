ec7715d88d5843d3e3069cd1e90a8ac294a508d3
jefron <jefron@1df66291-9572-4e14-8f18-50d9d9145325>
2012-04-10 15:06:13 +0000

    Add US Terms
    git-svn-id: https://csfe.aceworkspace.net/svn/repos/release-assertion-toolkit/trunk@204 1df66291-9572-4e14-8f18-50d9d9145325

--- release-assertion-resources/src/main/resources/datafiles/create_resource_tables.sql
+++ release-assertion-resources/src/main/resources/datafiles/create_resource_tables.sql
@@ -1,18 +1,23 @@
 create table res_navigationconcepts 
- (
- id	varchar(36),
- effectiveTime	varchar(255),
- active	varchar(255),
- moduleId	varchar(255),
- refsetId	varchar(255),
- referencedComponentId	varchar(255),
- ordernum	varchar(255),
- linkedTo	varchar(255)
- );
+(
+	 id		varchar(36),
+	 effectiveTime	varchar(255),
+	 active		varchar(255),
+	 moduleId	varchar(255),
+	 refsetId	varchar(255),
+	 referencedComponentId	varchar(255),
+	 ordernum	varchar(255),
+ 	linkedTo	varchar(255)
+);
  
- create table res_gbterms 
-  (
-  term	varchar(255)
-  );
+create table res_gbterms 
+(
+	term	varchar(255)
+);
   
+
+create table res_usterms 
+(
+	term	varchar(255)
+);
  
