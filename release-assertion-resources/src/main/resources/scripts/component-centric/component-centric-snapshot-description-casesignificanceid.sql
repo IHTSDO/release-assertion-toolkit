c281e4587b4d1d12fb99346c31ed704b8ca036b7
vparekh <vparekh@1df66291-9572-4e14-8f18-50d9d9145325>
2012-04-11 21:24:27 +0000

    git-svn-id: https://csfe.aceworkspace.net/svn/repos/release-assertion-toolkit/trunk@206 1df66291-9572-4e14-8f18-50d9d9145325

---
+++ release-assertion-resources/src/main/resources/scripts/component-centric/component-centric-snapshot-description-casesignificanceid.sql
@@ -0,0 +1,42 @@
+
+/******************************************************************************** 
+	component-centric-snapshot-description-casesignificanceid
+
+	Assertion:
+	Case-sensitive terms have appropriate caseSignificanceId.
+
+********************************************************************************/
+	
+/* 	view of current snapshot made by finding all the noncasesitive term for active concepts */
+	
+	create table t_curr_snapshot_1 as
+	select a.*
+	from  curr_description_d a , curr_concept_s b 
+	where a.casesignificanceid != 900000000000017005
+	and a.active = 1
+	and b.active = 1
+	and a.conceptid = b.id;
+
+	create index idx_term on t_curr_snapshot_1(term);
+	
+	create table t_curr_snapshot_2 as
+	select a.id
+	from  t_curr_snapshot_1 a , res_casesensitiveTerms b
+	where a.term like b.casesensitiveTerm  ;  
+	
+	
+	
+/* 	inserting exceptions in the result table */
+	insert into qa_result (runid, assertionuuid, assertiontext, details)
+	select 
+		<RUNID>,
+		'<ASSERTIONUUID>',
+		'<ASSERTIONTEXT>',
+		concat('DESC: id=',a.id, ':Case-sensitivity terms containing inappropriate caseSignificanceId.') 	
+	from t_curr_snapshot_2 a;
+
+
+	drop table t_curr_snapshot_1;
+	drop table t_curr_snapshot_2;
+
+	
