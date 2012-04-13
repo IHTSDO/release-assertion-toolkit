95f9a638a334d0c40c1723f8e63395a6ab79da92
vparekh <vparekh@1df66291-9572-4e14-8f18-50d9d9145325>
2012-04-13 19:15:06 +0000

    Resource Script.
    git-svn-id: https://csfe.aceworkspace.net/svn/repos/release-assertion-toolkit/trunk@215 1df66291-9572-4e14-8f18-50d9d9145325

--- release-assertion-resources/src/main/resources/scripts/special/create_resource_tables.sql
+++ release-assertion-resources/src/main/resources/scripts/special/create_resource_tables.sql
@@ -1,5 +1,4 @@
-create table res_navigationconcepts 
-(
+create table res_navigationconcepts (
 	 id		varchar(36),
 	 effectiveTime	varchar(255),
 	 active		varchar(255),
@@ -10,14 +9,24 @@ create table res_navigationconcepts
  	linkedTo	varchar(255)
 );
  
-create table res_gbterms 
-(
+create table res_gbterms (
 	term	varchar(255)
 );
   
 
-create table res_usterms 
-(
+create table res_usterms (
 	term	varchar(255)
 );
- 
+
+
+create table res_semantictags(
+   semantictag VARCHAR(255) not null,      
+   id VARCHAR(36) not null      
+);
+    
+create table res_casesensitiveTerms(
+   casesensitiveTerm VARCHAR(255) not null
+);
+create index idx_casesensitiveTerm on res_casesensitiveTerms(casesensitiveTerm);
+
+     
