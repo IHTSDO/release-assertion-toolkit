90afdb6c9ed716cd6a2f0f379956fc987a67bb0d
jefron <jefron@1df66291-9572-4e14-8f18-50d9d9145325>
2012-04-13 19:21:13 +0000

    Test every 4 different ways the statement 'Each concept has one and only one active preferred term in a given language refet' can be construed as a failure
    git-svn-id: https://csfe.aceworkspace.net/svn/repos/release-assertion-toolkit/trunk@216 1df66291-9572-4e14-8f18-50d9d9145325

--- release-assertion-resources/src/main/resources/scripts/file-centric/file-centric-snapshot-language-unique-pt.sql
+++ release-assertion-resources/src/main/resources/scripts/file-centric/file-centric-snapshot-language-unique-pt.sql
@@ -4,11 +4,17 @@
 
 	Assertion:
 	Every concept's Preferred Term exists exactly once in each language refset.
+	
+	Four Tests:
+	1) Concept has Preferred Term that is defined 2+ times for a given refset
+	2) Active Concept doesn't contain an active Synonym
+	3) All active Synonyms for active Concepts does not exist in the any language refset 
+	4) Concept does not have an active Preferred Term in each possible refset
 
 ********************************************************************************/
 	
-	
-	/* Concept has Preferred Term that is defined 2+ times for a given refset */
+        
+	/* TEST: Concept has Preferred Term that is defined 2+ times for a given refset */
 	insert into qa_result (runid, assertionuuid, assertiontext, details)
 	select 
 		<RUNID>,
@@ -16,50 +22,91 @@
 		'<ASSERTIONTEXT>',
 		concat('CONCEPT: id=',c.id, ': Concept has Preferred Term that is defined more than one time within a given refset.') 
 	from curr_description_s a 
-	inner join curr_langrefset_s b on a.id = b.referencedcomponentid 
+	INNER JOIN curr_langrefset_s b on a.id = b.referencedComponentid
 	inner join curr_concept_s c on a.conceptid = c.id
-	where c.active = '1'
+        where b.acceptabilityid = '900000000000548007'	
+	and c.active = '1'
 	and b.active = '1'
 	and a.active = '1'
-	and a.typeid = '900000000000548007'
 	GROUP BY b.referencedcomponentid, b.refsetid
-	having count(b.referencedcomponentid) > 1
+	having count(b.referencedcomponentid) > 1;
 	
 	
 	
-		
-	/* Concept does not have an Preferred Term defined */
+	
+	
+	/* TEST: Active Concept doesn't contain an active Synonym */
+	
+	/* View of all active synonyms */
+	create or replace view active_synonym_view as	
+		select a.conceptid from curr_description_s a
+		where a.typeid = '900000000000013009'
+		and a.active = '1';
+	
+	
+	/* Active Concepts without active synonyms */
 	insert into qa_result (runid, assertionuuid, assertiontext, details)
 	select 
 		<RUNID>,
 		'<ASSERTIONUUID>',
 		'<ASSERTIONTEXT>',
-		concat('CONCEPT: id=',a.id, ': Concept does not have an Preferred Term defined.') 
-	from curr_concept_s a 
-	inner join curr_description_s b on b.conceptid = a.id
-	where b.typeid = '900000000000548007'
-	having count(b.id) = 0
+			concat('CONCEPT: id=',a.id, ': Concept does not have an Preferred Term in any refset.') 
+	from curr_concept_s a
+	left join active_synonym_view b on b.conceptid = a.id
+	where a.active = '1'
+	and  b.conceptid is null;
+
+
+	drop view active_synonym_view;
 
 	
 	
-	/* Concept does not have an Preferred Term in any refset */
+	
+	
+	
+	
+	/* TEST: All active Synonyms for active Concepts does not exist in the any language refset */
+
+	/* Create view of all active Synonyms for all active concepts */
+	create or replace view active_con_syn_view as
+		select a.id from curr_description_s a
+		inner join curr_concept_s b on b.id = a.conceptid
+		where a.typeid = '900000000000013009'
+		and a.active = '1'
+		and b.active = '1';
+
+
+	/* Create view of all active Preferred Terms in the language refset table */
+	create or replace view active_preferred_view as
+		select a.referencedcomponentid from curr_langrefset_s a
+		where a.acceptabilityid = '900000000000548007'
+		and a.active = '1';
+
+		
+		
+	/* Synonym does not contain a an active Preferred Term in the language refset */
 	insert into qa_result (runid, assertionuuid, assertiontext, details)
 	select 
 		<RUNID>,
 		'<ASSERTIONUUID>',
 		'<ASSERTIONTEXT>',
-		concat('CONCEPT: id=',b.conceptid, ': Concept does not have an Preferred Term in any refset.') 
-	from curr_description_s a 
-	left join curr_langrefset_s b on a.id = b.referencedcomponentid 
-	inner join curr_concept_s c on a.conceptid = c.id
-	where b.id is null
-	and a.active = '1'	
-	and c.active = '1'
-	and a.typeid = '900000000000548007'
+			concat('CONCEPT: id=',a.id, ': Concept does not have an Preferred Term in any refset.') 
+	from active_con_syn_view a
+	left join active_preferred_view b on b.referencedcomponentid = a.id
+	where  b.referencedcomponentid is null;
+
+
+	drop view active_preferred_view;
+	drop view active_con_syn_view;
+		
+		
+		
+		
+		
 		
 	
 	
-	/* Concept does not have an Preferred Term in each possible refset */
+	/* TEST: Concept does not have an active Preferred Term in each possible refset */
 	select 
 		<RUNID>,
 		'<ASSERTIONUUID>',
@@ -71,8 +118,8 @@
 	where a.active = '1'
 	and b.active = '1'
 	and c.active = '1'
-	and b.typeid = '900000000000548007'
+	and a.acceptabilityid = '900000000000548007'
 	GROUP BY a.referencedcomponentid
-	having count(distinct(a.refsetid)) < (select count(distinct(refsetid)) from curr_langrefset_s)
+	having count(distinct(a.refsetid)) < (select count(distinct(refsetid)) from curr_langrefset_s);
 	
         
