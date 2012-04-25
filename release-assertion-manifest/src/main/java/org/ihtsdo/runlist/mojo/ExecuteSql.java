7ea511b5a199d11ac725a4808fedf7b18cb5cb9b
vparekh <vparekh@1df66291-9572-4e14-8f18-50d9d9145325>
2012-04-25 16:51:34 +0000

    Testing
    git-svn-id: https://csfe.aceworkspace.net/svn/repos/release-assertion-toolkit/trunk@295 1df66291-9572-4e14-8f18-50d9d9145325

--- release-assertion-manifest/src/main/java/org/ihtsdo/runlist/mojo/ExecuteSql.java
+++ release-assertion-manifest/src/main/java/org/ihtsdo/runlist/mojo/ExecuteSql.java
@@ -165,6 +165,9 @@ public class ExecuteSql extends AbstractMojo
 
 		if (f != null && !f.exists()) {
 			logger.logError("Script \"" + f.getAbsolutePath() + "\" doesn't exist (UUID: " + currentScript.getUuid().toString() + ")");
+			logger.logError(currentScript.getSqlFile());
+			logger.logError(currentScript.toString());
+			
 			return null;
 		}
 		
