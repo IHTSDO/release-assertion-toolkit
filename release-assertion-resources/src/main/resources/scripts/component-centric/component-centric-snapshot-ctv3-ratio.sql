1cc1cc4c428b2b783252fea233020cd1d152d89f
rturnbull <rturnbull@1df66291-9572-4e14-8f18-50d9d9145325>
2012-04-27 22:46:58 +0000

    git-svn-id: https://csfe.aceworkspace.net/svn/repos/release-assertion-toolkit/trunk@306 1df66291-9572-4e14-8f18-50d9d9145325

--- release-assertion-resources/src/main/resources/scripts/component-centric/component-centric-snapshot-ctv3-ratio.sql
+++ release-assertion-resources/src/main/resources/scripts/component-centric/component-centric-snapshot-ctv3-ratio.sql
@@ -3,27 +3,28 @@
 	component-centric-snapshot-ctv3-ratio
 
 	Assertion:
-	There is one CTV3 simple map refset members per concept.
+	There is one and only one CTV3 simple map refset members per concept.
 
 ********************************************************************************/
-	
-	
+		
 	/* TEST: Concept maps to multiple CTV3 Refset Members */
 	insert into qa_result (runid, assertionuuid, assertiontext, details)
 	select 
 		<RUNID>,
 		'<ASSERTIONUUID>',
 		'<ASSERTIONTEXT>',
+<<<<<<< .mine
+		concat('Concept: id=',b.id, ': Concept has more than one associated CTV3 refset member.') 
+=======
 		concat('CONCEPT: id=',b.id, ': Concept has more than one associated CTV3 refset member.') 
 	
+>>>>>>> .r257
 	from curr_simplemaprefset_s a
-	inner join curr_concept_s b on a.referencedcomponentid = b.id
+	inner join curr_concept_s b 
+		on a.referencedcomponentid = b.id
 	where a.refsetid = '900000000000497000'
 	group by a.id
 	having count(a.id) > 1;
-
-
-
 
 
 
@@ -41,10 +42,34 @@
 		<RUNID>,
 		'<ASSERTIONUUID>',
 		'<ASSERTIONTEXT>',
+<<<<<<< .mine
+		concat('Concept: id=',id, ': Concept does not have an associated CTV3 refset member.') 
+	from curr_concept_s a 
+		left join curr_simplemaprefset_s b
+		on a.id = b.referencedcomponentid
+		and b.referencedcomponentid is null
+	where b.refsetid = '900000000000497000';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+=======
 		concat('CONCEPT: id=',a.id, ': Concept does not have an associated CTV3 refset member.') 
 	
 	from curr_concept_s a
 	left join v_act_ctv3 b on a.id = b.referencedComponentId 
 	where b.referencedComponentId is null;
 
-	drop view v_act_ctv3;
+	drop view v_act_ctv3;>>>>>>> .r257
