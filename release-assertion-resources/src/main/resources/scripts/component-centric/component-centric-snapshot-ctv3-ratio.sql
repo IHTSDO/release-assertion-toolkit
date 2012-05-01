c2c925110efd51ca576c32ac922fe59228edfa5e
rturnbull <rturnbull@1df66291-9572-4e14-8f18-50d9d9145325>
2012-05-01 18:28:04 +0000

    git-svn-id: https://csfe.aceworkspace.net/svn/repos/release-assertion-toolkit/trunk@333 1df66291-9572-4e14-8f18-50d9d9145325

--- release-assertion-resources/src/main/resources/scripts/component-centric/component-centric-snapshot-ctv3-ratio.sql
+++ release-assertion-resources/src/main/resources/scripts/component-centric/component-centric-snapshot-ctv3-ratio.sql
@@ -3,48 +3,52 @@
 	component-centric-snapshot-ctv3-ratio
 
 	Assertion:
-	There is one CTV3 simple map refset members per concept.
+	There is one and only one CTV3 simple map refset member per concept.
 
 ********************************************************************************/
 	
 	
-	/* TEST: Concept maps to multiple CTV3 Refset Members */
+	/* Concept maps to multiple CTV3 Refset Members */
 	insert into qa_result (runid, assertionuuid, assertiontext, details)
 	select 
 		<RUNID>,
 		'<ASSERTIONUUID>',
 		'<ASSERTIONTEXT>',
-		concat('CONCEPT: id=',b.id, ': Concept has more than one associated CTV3 refset member.') 
-	
+		concat('CONCEPT: id=',a.referencedcomponentid, ': Concept has more than one associated CTV3 refset member.') 
 	from curr_simplemaprefset_s a
-	inner join curr_concept_s b on a.referencedcomponentid = b.id
 	where a.refsetid = '900000000000497000'
-	group by a.id
-	having count(a.id) > 1;
-
-
-
-
-
-
-	/* TEST: Concept is without a CTV3 Refset Member mapping */
-
-	/* Create view of all active CTV3 refset members */
-	create or replace view v_act_ctv3 as
-		select referencedComponentid 
+	group by a.referencedcomponentid
+	having count(a.referencedcomponentid) > 1;
+	commit;
+	
+	
+/* Create view of CTV3 refset members */
+	create or replace view v_ctv3 as
+		select referencedcomponentid 
 		from curr_simplemaprefset_s 
 		where refsetid = '900000000000497000';
 			
-
+/* Concept is without a CTV3 Refset Member mapping */
 	insert into qa_result (runid, assertionuuid, assertiontext, details)
 	select 
 		<RUNID>,
 		'<ASSERTIONUUID>',
 		'<ASSERTIONTEXT>',
 		concat('CONCEPT: id=',a.id, ': Concept does not have an associated CTV3 refset member.') 
-	
 	from curr_concept_s a
-	left join v_act_ctv3 b on a.id = b.referencedComponentId 
-	where b.referencedComponentId is null;
+	left join v_ctv3 b 
+		on a.id = b.referencedcomponentid 
+	where b.referencedcomponentid is null;
 
-	drop view v_act_ctv3;
+	drop view if exists v_act_ctv3;
+
+
+
+
+
+
+
+
+
+
+	
