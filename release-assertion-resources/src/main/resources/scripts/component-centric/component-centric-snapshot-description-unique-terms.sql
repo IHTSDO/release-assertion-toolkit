5e6d7d99b36f5de5a1fb054effd3a4031973cab1
rturnbull <rturnbull@1df66291-9572-4e14-8f18-50d9d9145325>
2012-05-07 21:01:34 +0000

    git-svn-id: https://csfe.aceworkspace.net/svn/repos/release-assertion-toolkit/trunk@367 1df66291-9572-4e14-8f18-50d9d9145325

--- release-assertion-resources/src/main/resources/scripts/component-centric/component-centric-snapshot-description-unique-terms.sql
+++ release-assertion-resources/src/main/resources/scripts/component-centric/component-centric-snapshot-description-unique-terms.sql
@@ -1,67 +1,50 @@
+use postqa;
 
 /******************************************************************************** 
 	component-centric-snapshot-description-unique-terms
 
 	Assertion:
-	Active Terms associated with active concepts are unique in Description hierarchy.
+	Active terms associated with active concepts are unique within hierarchy.
 
 ********************************************************************************/
 	
-/* 	view of current snapshot made by finding active terms associated with active concepts */
-	create or replace view v_curr_snapshot_1 as
-	select a.* 
-	from curr_description_s a , curr_concept_s b
-	where a.active = 1
-	and a.conceptid = b.id
-	and b.active = 1;
-	
-/* 	view of current snapshot made by matching active terms with inactive terms for active concepts */	
-	create or replace view v_curr_snapshot_2 as
-	select a.conceptid , a.term as synonym , c.term as fsn , a.id as synonymid, c.id as fsnid , SUBSTRING_INDEX(c.term, '(', -1) as hierarchytag
-	from curr_description_s a , curr_concept_s b , v_curr_snapshot_1 c
-	where a.active = 1
-	and b.active = 1
-	and c.typeid =  '900000000000003001'
-	and a.conceptid = b.id	
-	and a.conceptid = c.conceptid
-	and a.typeid != '900000000000003001';	
-	
-	create or replace view v_curr_snapshot_3 as
-	select b.fsn , b.hierarchytag  , b.fsnid
-	from res_semantictags a , v_curr_snapshot_2 b
-	where a.semantictag = replace(b.hierarchytag, ')' , '')
-	group by BINARY b.fsn , b.hierarchytag
-	having count(b.fsn) > 1 and count(b.hierarchytag) > 1;
-	
-	create or replace view v_curr_snapshot_4 as
-	select b.synonym , b.hierarchytag , b.synonymid
-	from res_semantictags a , v_curr_snapshot_2 b
-	where a.semantictag = replace(b.hierarchytag, ')' , '')
-	group by BINARY b.synonym , b.hierarchytag
-	having count(b.synonym) > 1 and count(b.hierarchytag) > 1;
+/* 	a list of concepts and their semantic tags, for active concepts edited this release */
+	drop temporary table if exists tmp_hierarchy;
+	create temporary table if not exists tmp_hierarchy as
+	select a.conceptid, concat('(',substring_index(a.term, '(', -1)) as semantictag
+	from curr_description_s a
+		join res_concepts_edited b
+			on a.conceptid = b.conceptid
+		join curr_concept_s c
+			on 	c.id = b.conceptid
+			and c.active = 1
+	where a.typeid = '900000000000003001' /* fully specified name */
+	and a.active = 1;
+	commit;	 
 
+/* 	a list of descriptions and their hierarchies */
+	drop temporary table if exists tmp_description; 
+	create temporary table if not exists tmp_description 
+	select a.id, a.term, b.semantictag as semantictag
+	from curr_description_s a
+		join tmp_hierarchy b
+			on a.conceptid = b.conceptid
+	where a.active = 1;
+	commit;
 
-/* 	inserting exceptions in the result table */
+/* 	violators to the results table */	
 	insert into qa_result (runid, assertionuuid, assertiontext, details)
 	select 
 		<RUNID>,
 		'<ASSERTIONUUID>',
 		'<ASSERTIONTEXT>',
-		concat('DESCRIPTION: term=',a.fsnid, ':Term not unique in Description hierarchy.') 	
-	from v_curr_snapshot_3 a ;
-
-/* 	inserting exceptions in the result table */
-	insert into qa_result (runid, assertionuuid, assertiontext, details)
-	select 
-		<RUNID>,
-		'<ASSERTIONUUID>',
-		'<ASSERTIONTEXT>',
-		concat('DESCRIPTION: term=',a.synonymid, ':Term not unique in Description hierarchy.') 	
-	from v_curr_snapshot_4 a ;
+		concat('DESC: id=',a.id, ':Term is not unique within hierarchy.') 
+	from tmp_description a
+	group by binary a.term, a.semantictag
+	having count(a.term) > 1
+	and count(a.semantictag) > 1;
+	commit;
 	
-
-	drop view v_curr_snapshot_1;
-	drop view v_curr_snapshot_2;
-	drop view v_curr_snapshot_3;	
-	drop view v_curr_snapshot_4;
-		
+	drop temporary table if exists tmp_hierarchy;
+	drop temporary table if exists tmp_description;
+	
