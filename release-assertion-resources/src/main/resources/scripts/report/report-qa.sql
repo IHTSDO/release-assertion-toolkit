68108e3f2e768172ff1517f4bff130ece3405325
rturnbull <rturnbull@1df66291-9572-4e14-8f18-50d9d9145325>
2012-05-10 22:13:07 +0000

    git-svn-id: https://csfe.aceworkspace.net/svn/repos/release-assertion-toolkit/trunk@390 1df66291-9572-4e14-8f18-50d9d9145325

--- release-assertion-resources/src/main/resources/scripts/report/report-qa.sql
+++ release-assertion-resources/src/main/resources/scripts/report/report-qa.sql
@@ -8,6 +8,9 @@
 ********************************************************************************/
 	
 		insert into qa_report (runid, assertionuuid, assertiontext, result ,count)
-		select b.runid , b.assertionuuid , b.assertiontext , 'F' , count(b.assertionuuid)  
-		from qa_result b 
-		where b.runid = <RUNID>;
+		select a.runid , b.assertionuuid , b.assertiontext , 'F' , count(b.assertionuuid)  
+		from qa_run a , qa_result b 
+		where a.runid = b.runid 
+		and a.runid = <RUNID>
+		group by b.assertionuuid
+		order by 2 desc;
