a16addd44900ee4d46f1ca80d4a37c1f1b14153b
vparekh <vparekh@1df66291-9572-4e14-8f18-50d9d9145325>
2012-11-30 17:48:31 +0000

    Fixed issue related to blank column present in the beginning or ending of the row.
    git-svn-id: https://csfe.aceworkspace.net/svn/repos/release-assertion-toolkit/trunk@514 1df66291-9572-4e14-8f18-50d9d9145325

--- release-file-qa/src/main/java/org/ihtsdo/release/fileqa/tests/ColumnDataTests.java
+++ release-file-qa/src/main/java/org/ihtsdo/release/fileqa/tests/ColumnDataTests.java
@@ -49,10 +49,9 @@ public class ColumnDataTests {
 
 			// skip the header row
 			while (lineCount < 1) {
-
 				headerData = br.readLine();
 				if (headerData != null)
-					headerSize = headerData.length();
+					headerSize = headerData.length();					
 				if (logger.isDebugEnabled()) {
 					logger.debug("Header :" + headerData);
 					logger.debug("Size :" + headerSize);
@@ -60,27 +59,125 @@ public class ColumnDataTests {
 				}
 				lineCount++;
 			}
-
+			
+			
 			// read the file
-			String lineData = null;
-
+			String lineData = null;			
 			while ((lineData = br.readLine()) != null) {
-
 				lineCount++;
+				int beginIndex = 0;
+				char emptyStartChar =lineData.charAt(0);	
+				if(emptyStartChar == '\t'){
+					//first element is empty execute regex test handling as special loop					
+					// test the column data using regex expressions
+					if (qa.getColumn().get(beginIndex).getRegex() != null) {						
+						//loop across the regex tests	
+						for (int regexCount = 0; regexCount < qa.getColumn()
+								.get(beginIndex).getRegex().size()
+								; regexCount++) {
+							boolean match = false;
+							try {
+								match = Character.toString(emptyStartChar).matches(qa.getColumn().get(
+										beginIndex).getRegex().get(regexCount).getExpression());
+							} catch (PatternSyntaxException e) {
+								writeExcel.addRow(MessageType.FAILURE, qa
+										.getColumn().get(beginIndex).getRegex()
+										.get(regexCount).getTest()
+										+ ",Current,Failed,Line No. "
+										+ lineCount
+										+ " : Column :"
+										+ qa.getColumn().get(beginIndex).getHeader()
+										+ " Data: "
+										+ emptyStartChar + " :" + e.getMessage());
+							}
 
+							if (!match) {
+								// look if this type of error happened before
+								boolean found = findError(qa.getColumn().get(beginIndex).getRegex().get(regexCount).getTest(), 
+										qa.getColumn().get(beginIndex).getHeader(), 
+										qa.getColumn().get(beginIndex).getRegex().get(regexCount).getExpression());
+								if (!found) {
+									TestError error = new TestError();
+									error.setTest(qa.getColumn().get(beginIndex).getRegex().get(regexCount).getTest());
+									error.setColumnHeader(qa.getColumn().get(beginIndex).getHeader());
+									error.setRegex(qa.getColumn().get(beginIndex).getRegex().get(regexCount).getExpression());
+									error.setMessage(qa.getColumn().get(beginIndex).getRegex().get(regexCount).getFailureMessage());
+									error.setLineCount(lineCount);
+									error.setColumnData(Character.toString(emptyStartChar));
+									error.setCount(1);
+									errors.add(error);
+								}
+							}
+						} // end looping thru regex tests
+					} // end if regex not null					
+				}
+				
+				
+				
+				
+				boolean emptyEndCharFlag = lineData.endsWith("\t");						
+				if(emptyEndCharFlag){
+					char emptyEndChar ='\t';
+					int lastIndex = qa.getColumn().size() - 1;
+					//last element is empty execute regex test handling as special loop					
+					// test the column data using regex expressions
+					if (qa.getColumn().get(lastIndex).getRegex() != null) {						
+						//loop across the regex tests	
+						for (int regexCount = 0; regexCount < qa.getColumn().get(lastIndex).getRegex().size()
+								; regexCount++) {
+							boolean match = false;
+							try {
+								match = Character.toString(emptyEndChar).matches(qa.getColumn().get(
+										lastIndex).getRegex().get(regexCount).getExpression());
+							} catch (PatternSyntaxException e) {
+								writeExcel.addRow(MessageType.FAILURE, qa
+										.getColumn().get(lastIndex).getRegex()
+										.get(regexCount).getTest()
+										+ ",Current,Failed,Line No. "
+										+ lineCount
+										+ " : Column :"
+										+ qa.getColumn().get(lastIndex).getHeader()
+										+ " Data: "
+										+ emptyEndChar + " :" + e.getMessage());
+							}
+
+							if (!match) {
+								// look if this type of error happened before
+								boolean found = findError(qa.getColumn().get(lastIndex).getRegex().get(regexCount).getTest(), 
+										qa.getColumn().get(lastIndex).getHeader(), 
+										qa.getColumn().get(lastIndex).getRegex().get(regexCount).getExpression());
+								if (!found) {
+									TestError error = new TestError();
+									error.setTest(qa.getColumn().get(lastIndex).getRegex().get(regexCount).getTest());
+									error.setColumnHeader(qa.getColumn().get(lastIndex).getHeader());
+									error.setRegex(qa.getColumn().get(lastIndex).getRegex().get(regexCount).getExpression());
+									error.setMessage(qa.getColumn().get(lastIndex).getRegex().get(regexCount).getFailureMessage());
+									error.setLineCount(lineCount);
+									error.setColumnData(Character.toString(emptyEndChar));
+									error.setCount(1);
+									errors.add(error);
+								}
+							}
+						} // end looping thru regex tests
+					} // end if regex not null					
+				}				
+				
 				Scanner lineScanner = new Scanner(lineData);
 				lineScanner.useDelimiter(qa.getFile().getDelimiter());
-
+				
 				int column = 0;
+				
+				if(emptyStartChar == '\t'){	
+					column = 1; //First column is empty so start from next column
+				}
+					
 				boolean rowEmpty = true;
 				boolean lineLoop = true;
 				
 				// process the read row
-				while (lineScanner.hasNext() && lineLoop) {
-
+				while (lineScanner.hasNext() && lineLoop) {					
 					String columnData = lineScanner.next();
-					boolean loop = true;
-					
+					boolean loop = true;					
 
 					if ( column < qa.getColumn().size()) {
 					// column or row trailing spaces test
@@ -90,7 +187,6 @@ public class ColumnDataTests {
 						boolean found = findError("ColumnTrailingSpacesTest",
 								qa.getColumn().get(column).getHeader(),
 								"ColumnTrailingSpacesTest");
-
 						if (!found) {
 							TestError error = new TestError();
 
@@ -115,9 +211,8 @@ public class ColumnDataTests {
 							for (int regexCount = 0; regexCount < qa.getColumn()
 									.get(column).getRegex().size()
 									&& loop; regexCount++) {
-	
 								boolean match = false;
-								try {
+								try {								
 									match = columnData.matches(qa.getColumn().get(
 											column).getRegex().get(regexCount)
 											.getExpression());
@@ -210,6 +305,7 @@ public class ColumnDataTests {
 								}
 							} // end looping thru regex tests
 						} // end if regex not null
+											
 
 						if (qa.getColumn().get(column).getHeader()
 								.toLowerCase().equals("effectivetime")) {
@@ -420,14 +516,14 @@ public class ColumnDataTests {
 				if (error.getCount() > 1) {
 					if (!error.getColumnHeader().equals("Blank"))
 						writeExcel.addRow(MessageType.FAILURE, error.getTest()
-								+ ",Current,Failed,Total lines :"
+								+ ",Current,Failed,Total errors :"
 								+ error.getCount() + " Starting line no. :"
 								+ error.getLineCount() + " Column :"
 								+ error.getColumnHeader() + " : "
 								+ error.getMessage());
 					else
 						writeExcel.addRow(MessageType.FAILURE, error.getTest()
-								+ ",Current,Failed,Total lines :"
+								+ ",Current,Failed,Total errors :"
 								+ error.getCount() + " Starting line no. :"
 								+ error.getLineCount() + "  : "
 								+ error.getMessage());
--- release-file-qa/src/main/java/org/ihtsdo/release/fileqa/tests/FileSizeTest.java
+++ release-file-qa/src/main/java/org/ihtsdo/release/fileqa/tests/FileSizeTest.java
@@ -1,7 +1,10 @@
 package org.ihtsdo.release.fileqa.tests;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.security.MessageDigest;
 
 import org.apache.log4j.Logger;
 import org.ihtsdo.release.fileqa.model.MessageType;
@@ -21,10 +24,40 @@ public class FileSizeTest {
 
 		return fileSize;
 	}
+	
+	public static byte[] createChecksum(String filename) throws Exception {
+	       InputStream fis =  new FileInputStream(filename);
+	       byte[] buffer = new byte[1024];
+	       MessageDigest complete = MessageDigest.getInstance("MD5");
+	       int numRead;
+
+	       do {
+	           numRead = fis.read(buffer);
+	           if (numRead > 0) {
+	               complete.update(buffer, 0, numRead);
+	           }
+	       } while (numRead != -1);
+
+	       fis.close();
+	       return complete.digest();
+	   }
+
+	
+	
+	   // a byte array to a HEX string
+	   public static String getMD5Checksum(String filename) throws Exception {
+	       byte[] b = createChecksum(filename);
+	       String result = "";
+
+	       for (int i=0; i < b.length; i++) {
+	           result += Integer.toString( ( b[i] & 0xff ) + 0x100, 16).substring( 1 );
+	       }
+	       return result;
+	   }
 
 	public static boolean execute(final Metadata qa, final File currFile,
 			File prevFile, final Logger logger, final WriteExcel writeExcel)
-			throws IOException {
+			throws IOException , Exception{
 
 		boolean passed = false;
 
@@ -56,18 +89,27 @@ public class FileSizeTest {
 			}
 
 			if (logger.isDebugEnabled())
-				logger
-						.debug("Previous and Current Release File Size DONT match");
-		} else {
-			if (qa.getFile().getCarryForward().equals("false")) {
-				writeExcel.addRow(MessageType.FAILURE,"FileSizeTest,Current,Failed,Carry forward set to FALSE Bytes: "
-								+ currFileSize
-								+ " Size do match,"
-								+ currFile.getAbsoluteFile());
-				writeExcel.addRow(MessageType.FAILURE,"FileSizeTest,Previous,Failed,Carry forward set to FASLE Bytes: "
-								+ prevFileSize
-								+ " Size do match,"
-								+ prevFile.getAbsoluteFile());
+				logger.debug("Previous and Current Release File Size DONT match");
+			
+		} else {		
+			
+			String prevHashCode = getMD5Checksum(prevFile.getAbsoluteFile().toString());
+			String currHashCode = getMD5Checksum(currFile.getAbsoluteFile().toString());
+			
+			if(prevHashCode.equals(currHashCode)){
+				System.out.println(prevFile.getAbsoluteFile().toString() + " & " + prevHashCode);
+				System.out.println(currFile.getAbsoluteFile().toString() + " & " + currHashCode);
+			
+				if (qa.getFile().getCarryForward().equals("false")) {
+					writeExcel.addRow(MessageType.FAILURE,"FileSizeTest,Current,Failed,Carry forward set to FALSE Bytes: "
+									+ currFileSize
+									+ " Size do match,"
+									+ currFile.getAbsoluteFile());
+					writeExcel.addRow(MessageType.FAILURE,"FileSizeTest,Previous,Failed,Carry forward set to FASLE Bytes: "
+									+ prevFileSize
+									+ " Size do match,"
+									+ prevFile.getAbsoluteFile());
+				}
 			} else {
 				writeExcel.addRow(MessageType.SUCCESS,"FileSizeTest,Current,Passed, ,"
 						+ currFile.getAbsoluteFile());
@@ -76,6 +118,7 @@ public class FileSizeTest {
 			}
 			if (logger.isDebugEnabled())
 				logger.debug("Previous and Current Release File Size DO match");
+			
 			passed = true;
 		}
 
